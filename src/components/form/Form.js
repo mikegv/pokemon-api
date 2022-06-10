8157e0b66eb877f8c0bf05ac4b5d26ebc120aa11
mikegv <xmik3yvx@live.com>
2022-06-10 10:35:20 -0700

    first commit

---
+++ src/components/form/Form.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <div>
+            My Form
+        </div>
+    );
+};
+
+export default Form;
---
+++ src/components/results/Results.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Results = () => {
+    return (
+        <div>
+            my results
+        </div>
+    );
+};
+
+export default Results;
